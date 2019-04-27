33e627759fe6c258fde429b09fb84ed51c8ea427
Lauren Forcey <lauren4c@gmail.com>
2019-04-27 10:27:42 -0600

    Heroku Server deploy take 2

--- server.js
+++ server.js
@@ -1,41 +1,25 @@
-const app = require("./src/app");
-const http = require("http");
 const express = require("express");
-const appExpress = express();
+const app = express();
 const path = require("path");
 const bodyParser = require("body-parser");
+const appConfig = require("./src/config/main-config.js");
+const routeConfig = require("./src/config/route-config.js");
 
-const port = normalizePort(process.env.PORT || "5000");
-appExpress.use(bodyParser.urlencoded({ extended: true }));
+const port = process.env.PORT || "5000";
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.set("port", port);
-
-const server = http.createServer(app);
-
-server.listen(port);
-appExpress.use(express.static(__dirname));
+app.use(express.static(__dirname));
 
 if (process.env.NODE_ENV === "production") {
-  appExpress.use(express.static(path.join(__dirname, "shelter-fe/build")));
+  app.use(express.static(path.join(__dirname, "shelter-fe/build")));
   //
-  appExpress.get("*", (req, res) => {
-    res.sendfile(path.join(__dirname, "shelter-fe/build/index.html"));
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "shelter-fe/build", "index.html"));
   });
 }
 
-function normalizePort(val) {
-  const port = parseInt(val, 10);
-  if (isNaN(port)) {
-    return val;
-  }
-  if (port >= 0) {
-    return port;
-  }
-  return false;
-}
+appConfig.init(app, express);
+routeConfig.init(app);
 
-server.on("listening", () => {
-  console.log(
-    `server is listening for requests on port ${server.address().port}`
-  );
-});
+app.listen(port, () => console.log(`Listening on port ${port}`));
